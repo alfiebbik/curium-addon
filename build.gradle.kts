ee44c772dde57843145f71a497c17d8c64a27949
alfiebbik <sonicspeedyboy101@gmail.com>
2026-05-17 23:56:22 +0000

    21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,39 +6,6 @@ plugins {
 version = "1.0.0"
 group = "com.example.addon"
 
-loom {
-    splitEnvironmentSourceSets()
-    mods {
-        register("addon") {
-            sourceSet(sourceSets.main.get())
-        }
-    }
-}
-
-repositories {
-    mavenCentral()
-    maven {
-        name = "Fabric"
-        url = uri("https://maven.fabricmc.net/")
-    }
-    maven {
-        name = "Meteor Releases"
-        url = uri("https://maven.meteordev.org/releases")
-    }
-    maven {
-        name = "Meteor Snapshots"
-        url = uri("https://maven.meteordev.org/snapshots")
-    }
-    maven {
-        name = "TerraformersMC"
-        url = uri("https://maven.terraformersmc.com/releases/")
-    }
-    maven {
-        name = "Mojang"
-        url = uri("https://libraries.minecraft.net/")
-    }
-}
-
 dependencies {
     minecraft("com.mojang:minecraft:1.21.1")
     mappings(loom.officialMojangMappings())
@@ -46,9 +13,8 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.102.0+1.21.1")
 
-    // The mandatory layout required to compile Meteor Client files cleanly
+    // Injected safely through central dependency management
     implementation("meteordevelopment:meteor-client:0.5.8-SNAPSHOT")
-    annotationProcessor("meteordevelopment:meteor-client:0.5.8-SNAPSHOT")
 }
 
 tasks.withType<JavaCompile> {
