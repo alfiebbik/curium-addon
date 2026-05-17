2696d297002f35f2cf679a1cf585bb1c72d3b4c9
alfiebbik <sonicspeedyboy101@gmail.com>
2026-05-17 22:45:51 +0000

    1

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,49 +1,31 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.example.addon.modules.BetaFreecam;
 import com.mojang.logging.LogUtils;
-import meteordevelopment.meteorclient.addons.GithubRepo;
-import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
-import meteordevelopment.meteorclient.systems.modules.Category;
-import meteordevelopment.meteorclient.systems.modules.Modules;
+import minecraft.meteorclient.addons.MeteorAddon;
+import minecraft.meteorclient.systems.modules.Modules;
+import minecraft.meteorclient.systems.modules.categories.Render;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+public class Addon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing My Custom Addon!");
 
         // Modules
-        Modules.get().add(new ModuleExample());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        Modules.get().add(new BetaFreecam(Modules.get().getGroup(Render.class)));
     }
 
     @Override
     public void onRegisterCategories() {
-        Modules.registerCategory(CATEGORY);
+        // If you want to make your own custom GUI tab later, you do it here.
+        // For now, we are putting Freecam into Meteor's standard RENDER tab.
     }
 
     @Override
     public String getPackage() {
         return "com.example.addon";
-    }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
     }
 }
---
+++ src/main/java/com/example/addon/modules/BetaFreecam.java
@@ -0,0 +1,61 @@
+package com.example.addon.modules;
+
+import minecraft.meteorclient.systems.modules.Category;
+import minecraft.meteorclient.systems.modules.Module;
+import minecraft.meteorclient.events.world.TickEvent;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.entity.Entity;
+
+public class BetaFreecam extends Module {
+    private OtherClientPlayerEntity fakePlayer;
+    private double oldX, oldY, oldZ;
+
+    public BetaFreecam(Category category) {
+        super(category, "beta-freecam", "Fly out of your body to look around!");
+    }
+
+    @Override
+    public void onActivate() {
+        if (mc.player == null || mc.world == null) {
+            this.toggle();
+            return;
+        }
+
+        oldX = mc.player.getX();
+        oldY = mc.player.getY();
+        oldZ = mc.player.getZ();
+
+        fakePlayer = new OtherClientPlayerEntity(mc.world, mc.player.getGameProfile());
+        fakePlayer.copyFrom(mc.player);
+        fakePlayer.refreshPositionAndAngles(oldX, oldY, oldZ, mc.player.getYaw(), mc.player.getPitch());
+        fakePlayer.resetPosition();
+        mc.world.addEntity(-1337, fakePlayer);
+
+        info("Freecam active. Your body is safe!");
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        if (mc.player == null) return;
+
+        mc.player.getAbilities().flying = true;
+        mc.player.noClip = true;
+    }
+
+    @Override
+    public void onDeactivate() {
+        if (mc.player == null || mc.world == null) return;
+
+        if (fakePlayer != null) {
+            mc.world.removeEntity(-1337, Entity.RemovalReason.DISCARDED);
+        }
+
+        mc.player.setPosition(oldX, oldY, oldZ);
+        mc.player.getAbilities().flying = false;
+        mc.player.noClip = false;
+        mc.player.setVelocity(0, 0, 0);
+
+        info("Returned to your body.");
+    }
+}
