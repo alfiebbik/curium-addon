c31f8b8364e8010065897b6daeb6c5511a4b38ba
alfiebbik <sonicspeedyboy101@gmail.com>
2026-05-17 23:55:12 +0000

    21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,20 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
+        maven("https://maven.fabricmc.net/")
         mavenCentral()
         gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.meteordev.org/releases")
+        maven("https://maven.meteordev.org/snapshots")
+        maven("https://maven.terraformersmc.com/releases/")
+        maven("https://libraries.minecraft.net/")
     }
 }
 
